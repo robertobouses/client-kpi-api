1766b553a59f7c8ac381f911a2362575cdf2ef00
Roberto Fernandez Toro <robertobouses@gmail.com>
2025-01-22 17:39:03 +0100

    TestCalculateClientsKPI

--- app/create_clients_test.go
+++ app/create_clients_test.go
@@ -4,38 +4,15 @@ import (
 	"testing"
 	"time"
 
-	"github.com/gin-gonic/gin"
-	"github.com/google/uuid"
 	"github.com/robertobouses/client-kpi-api/app"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 )
 
-type MockClientRepo struct {
-	mock.Mock
-}
-
 func (m *MockClientRepo) InsertClients(req app.Client) error {
 	args := m.Called(req)
 	return args.Error(0)
 }
-
-func (m *MockClientRepo) QueryAllClients() ([]app.Client, error) {
-	panic("not implemented")
-}
-func (m *MockClientRepo) QueryClientById(id uuid.UUID) (app.Client, error) {
-	panic("not implemented")
-}
-func (m *MockClientRepo) UpdateClientById(ctx *gin.Context, id uuid.UUID, req app.UpdateClientRequest) error {
-	panic("not implemented")
-}
-func (m *MockClientRepo) DeleteClientsById(id uuid.UUID) error {
-	panic("not implemented")
-}
-func (m *MockClientRepo) ClientExistsById(id uuid.UUID) (bool, error) {
-	panic("not implemented")
-}
-
 func TestCreateClients(t *testing.T) {
 	tests := []struct {
 		name          string
